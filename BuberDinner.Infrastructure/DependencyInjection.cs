ab480051a63d866d8fc25b485eaa2d5684467c30
Dheeraj Shrestha <dheeraj@crossovernepal.com>
2023-07-31 13:06:54 +0545

    Implement JwtTokenGenerator

--- BuberDinner.Infrastructure/DependencyInjection.cs
+++ BuberDinner.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,20 @@
-namespace BuberDinner.Infrastructure;
+ namespace BuberDinner.Infrastructure;
 
+using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Services;
+using BuberDinner.Infrastructure.Autentication;
+using BuberDinner.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+        ConfigurationManager configuration)
     {
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+        services.AddSingleton<IJwtTokengenerator, JwtTokenGenerator>();
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
         return services;
     }
 }
