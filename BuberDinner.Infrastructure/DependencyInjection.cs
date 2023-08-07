f741b8d8eac685c2204b7d93209401e98aa3084c
Dheeraj Shrestha <djshres@outlook.com>
2023-08-07 11:18:31 +0545

    JWT Bearer Authentication

--- BuberDinner.Infrastructure/DependencyInjection.cs
+++ BuberDinner.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,4 @@
- namespace BuberDinner.Infrastructure;
+namespace BuberDinner.Infrastructure;
 
 using BuberDinner.Application.Common.Interfaces.Authentication;
 using BuberDinner.Application.Common.Interfaces.Services;
@@ -6,18 +6,45 @@ using BuberDinner.Application.Persistence;
 using BuberDinner.Infrastructure.Authentication;
 using BuberDinner.Infrastructure.Persistence;
 using BuberDinner.Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services,
         ConfigurationManager configuration)
     {
-        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
-        services.AddSingleton<IJwtTokengenerator, JwtTokenGenerator>();
+        services.AddAuth(configuration);
         services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
         services.AddScoped<IUserRepository, UserRepository>();
+        return services;
+    }
+
+    public static IServiceCollection AddAuth(this IServiceCollection services,
+       ConfigurationManager configuration)
+    {
+        var JwtSettings = new JwtSettings();
+        configuration.Bind(JwtSettings.SectionName, JwtSettings);
+
+        services.AddSingleton(Options.Create(JwtSettings));
+        services.AddSingleton<IJwtTokengenerator, JwtTokenGenerator>();
+
+        services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = JwtSettings.Issuer,
+                ValidAudience = JwtSettings.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(JwtSettings.Secret))
+            });
         return services;
     }
 }
